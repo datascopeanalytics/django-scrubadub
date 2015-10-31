20147ae1d8949ec1faff9203d726eb9a62df2b52
Dean Malmgren <dean.malmgren@datascopeanalytics.com>
2015-10-31 14:13:38 -0500

    added initial models

---
+++ django_scrubadub/migrations/0001_initial.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Document',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('text', models.TextField()),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Filth',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('beg', models.IntegerField()),
+                ('end', models.IntegerField()),
+                ('text', models.CharField(max_length=255)),
+                ('document', models.ForeignKey(to='django_scrubadub.Document')),
+            ],
+        ),
+        migrations.CreateModel(
+            name='FilthType',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=255)),
+                ('detector_cls', models.CharField(max_length=255)),
+                ('filth_cls', models.CharField(max_length=255)),
+                ('parameters', models.TextField()),
+            ],
+        ),
+        migrations.AddField(
+            model_name='filth',
+            name='type',
+            field=models.ForeignKey(to='django_scrubadub.FilthType'),
+        ),
+    ]
--- django_scrubadub/models.py
+++ django_scrubadub/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
+class Document(models.Model):
+    """The Document stores the raw text of the document that needs to be
+    scrubbed
+    """
+
+    text = models.TextField()
+
+
+class FilthType(models.Model):
+    """Each type of filth is stored independently in the database including, as
+    necessary, any parameters of the filth.
+    """
+
+    name = models.CharField(max_length=255)
+    detector_cls = models.CharField(max_length=255)
+    filth_cls = models.CharField(max_length=255)
+    parameters = models.TextField()
+
+
+class Filth(models.Model):
+    """This stores the relevant information for each piece of filth, including
+    the document in which it appears and its FilthType
+    """
+
+    beg = models.IntegerField()
+    end = models.IntegerField()
+    text = models.CharField(max_length=255)
+    document = models.ForeignKey(Document)
+    type = models.ForeignKey(FilthType)
