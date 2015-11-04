5ac2eb23bbf61a5bdf5dbc9a1922f848376e5a03
Dean Malmgren <dean.malmgren@datascopeanalytics.com>
2015-11-04 14:05:37 -0600

    rudimentary filth type selection

--- django_scrubadub/static/django_scrubadub/js/document_detail.js
+++ django_scrubadub/static/django_scrubadub/js/document_detail.js
@@ -1,15 +1,31 @@
+var source = $("#filth-type").html();
+var template = Handlebars.compile(source);
+
+$(".scrubadub.document span").popover({
+    content: template(),
+    html: true,
+})
+
 $(".scrubadub.document span").click(function (event) {
-    var filth = $(this).data();
-    filth.text = $(this).text();
+    var filth_el = this;
+    var filth = $(filth_el).data();
+    filth.text = $(filth_el).text();
     filth.end = filth.beg + filth.text.length
 
-    // TODO: have user choose the type of filth that this is
-
-
     // visually indicate that this is filth
-    $(this).addClass('filth');
+    $(filth_el).addClass('filth');
 
-    // TODO: send filth to backend
+    // on selecting a filth type, bind filth with the function that hides
+    var popover_id = $(filth_el).attr('aria-describedby');
+    $('#' + popover_id + ' button').click(function (event) {
+        $(filth_el).popover('hide')
+
+        // style and store the filth type on the original element
+        $(filth_el).attr('data-filth_type', $(this).data('filth_type'));
+        filth.type = $(this).data('filth_type')
+
+        // TODO: send selection to backend
+    });
 
 
 
