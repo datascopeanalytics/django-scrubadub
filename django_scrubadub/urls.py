05b0a01eef74ea6025e4e143813d02cb6bf54158
Dean Malmgren <dean.malmgren@datascopeanalytics.com>
2015-11-07 05:25:19 -0600

    using the django rest framework in a minimal way to display documents

--- django_scrubadub/urls.py
+++ django_scrubadub/urls.py
@@ -1,10 +1,16 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from rest_framework.routers import DefaultRouter
+
 from . import views
+
+# this creates the urls using the rest framework's router mechanism
+# http://www.django-rest-framework.org/tutorial/6-viewsets-and-routers/#using-routers
+router = DefaultRouter()
+router.register(r'documents', views.DocumentViewSet)
 
 
 urlpatterns = [
-    url(r'^$', views.DocumentListView.as_view()),
-    url(r'^(?P<pk>\d+)/$', views.DocumentView.as_view()),
+    url(r'^', include(router.urls)),
 ]
--- django_scrubadub/views.py
+++ django_scrubadub/views.py
@@ -1,12 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render_to_response
 from django.views.generic import ListView, DetailView
 
+from rest_framework import viewsets, renderers
+from rest_framework.decorators import detail_route
+
 from .models import Document
+from .serializers import DocumentSerializer
 
 
-class DocumentListView(ListView):
-    model = Document
+class DocumentViewSet(viewsets.ModelViewSet):
+    queryset = Document.objects.all()
+    serializer_class = DocumentSerializer
 
-
-class DocumentView(DetailView):
-    model = Document
+    # this exposes an endpoint of the api in which you can review a particular
+    # document
+    @detail_route(renderer_classes=[renderers.StaticHTMLRenderer])
+    def review(self, request, *args, **kwargs):
+        document = self.get_object()
+        return render_to_response('django_scrubadub/document_review.html', {
+            "object": document,
+        })
