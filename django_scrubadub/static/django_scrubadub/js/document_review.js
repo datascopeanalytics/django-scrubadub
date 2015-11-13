f32042fedead6172f8c43be29175a30d94574d01
Dean Malmgren <dean.malmgren@datascopeanalytics.com>
2015-11-12 21:35:47 -0600

    silly popover

--- django_scrubadub/static/django_scrubadub/js/document_review.js
+++ django_scrubadub/static/django_scrubadub/js/document_review.js
@@ -1,16 +1,18 @@
 (function () {
     'use strict';
 
-    angular.module('scrubadub', ['onSelect'])
+    angular.module('scrubadub', ['onSelect', 'ngAnimate', 'ui.bootstrap.popover', 'ui.bootstrap.tpls'])
         .controller('ScrubadubController', ScrubadubController);
 
 
     function ScrubadubController($scope) {
         var vm = this;
-        vm.tktk = tktk;
+        vm.handleSelection = handleSelection;
         return vm;
 
-        function tktk(selection) {
+        function handleSelection(selection) {
+            selection.highlight('<span uib-popover="tktk" popover-is-open="true"></span>');
+
             console.log(selection.getText());
             // selection.highlight('<b></b>');
             // selection.removeHighlight();
