ff269dfa1d1f221fdb24c5bed64e622de3e12bf4
Dean Malmgren <dean.malmgren@datascopeanalytics.com>
2015-11-12 21:09:53 -0600

    the scrubadub controller....is ALIVE!

--- django_scrubadub/static/django_scrubadub/js/document_review.js
+++ django_scrubadub/static/django_scrubadub/js/document_review.js
@@ -1,1 +1,21 @@
-console.log('look ma')
+(function () {
+    'use strict';
+
+    angular.module('scrubadub', ['onSelect'])
+        .controller('ScrubadubController', ScrubadubController);
+
+
+    function ScrubadubController($scope) {
+        var vm = this;
+        vm.tktk = tktk;
+        return vm;
+
+        function tktk(selection) {
+            console.log(selection.getText());
+            // selection.highlight('<b></b>');
+            // selection.removeHighlight();
+        }
+
+    }
+
+})();
