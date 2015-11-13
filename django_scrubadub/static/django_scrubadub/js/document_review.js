58b27ab9d793135d49ef205a64f57d62bd14d2e4
Dean Malmgren <dean.malmgren@datascopeanalytics.com>
2015-11-12 22:55:28 -0600

    recording that filth was selected

--- django_scrubadub/static/django_scrubadub/js/document_review.js
+++ django_scrubadub/static/django_scrubadub/js/document_review.js
@@ -5,19 +5,41 @@
         .controller('ScrubadubController', ScrubadubController);
 
 
-    function ScrubadubController(RangeService) {
+    function ScrubadubController(RangeService, $scope) {
         var vm = this;
         vm.handleSelection = handleSelection;
         vm.templateUrl = '/static/django_scrubadub/templates/popover.html';
+        vm.onFilthSelected = onFilthSelected;
+        vm.currentSelection = undefined;
+        vm.popoverShown = [];
+
         return vm;
 
         function handleSelection(selection) {
-            RangeService.disabled = true;
-            selection.highlight('<span uib-popover-template="vm.templateUrl" popover-is-open="true"></span>');
+            // TODO: Make Clark fix this
+            if (RangeService.disabled) {
+                return;
+            }
 
-            console.log(selection.getText());
+            vm.popoverShown.push(true);
+            var isOpen = "vm.popoverShown["+(vm.popoverShown.length-1)+"]";
+
+            RangeService.disabled = true;
+            vm.currentSelection = selection;
+            selection.highlight('<span class="filth" uib-popover-template="vm.templateUrl" popover-is-open="'+isOpen+'"></span>');
+
             // selection.highlight('<b></b>');
             // selection.removeHighlight();
+        }
+
+        function onFilthSelected() {
+            console.log(vm.currentSelection)
+            console.log('we have determined that', vm.currentSelection.getText());
+            console.log('   is a filthy little ', vm.filthType);
+
+            vm.popoverShown[vm.popoverShown.length-1] = false;
+            RangeService.disabled = false;
+            // vm.currentSelection.removeHighlight();
         }
 
     }
